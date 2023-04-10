936abec5131b061ea5eab5f9c75fe102a089caba
Ruslan <trud555@ukr.net>
2023-04-10 11:36:57 +0300

    n8

--- index.js
+++ index.js
@@ -1,8 +1,14 @@
 const TelegramBot = require('node-telegram-bot-api');
+const express = require('express');
+const cors = require('cors');
+
 
 const token = '6114679288:AAGpRbAUZhy02NdiOt_wCxQwVTaxVzxwD84'
 
 const bot = new TelegramBot(token, {polling: true})
+const app = express()
+app.use(express.json())
+app.use(cors())
 
 const webAppUrl = 'https://bright-mermaid-f9479f.netlify.app/'
 
@@ -33,3 +39,6 @@ bot.on('message', async (msg) => {
 
   
 });
+
+const PORT = 8000
+app.listen(PORT, () => console.log('бот запущен'))
